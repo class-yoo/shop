9cfe84181ed1617af872e865553052610029d9db
class-yoo <mynameisyjh@gmail.com>
2025-03-25 23:09:09 +0900

    Init project

---
+++ src/main/kotlin/com/shoptest/ShopTestApplication.kt
@@ -0,0 +1,11 @@
+package com.shoptest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShopTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShopTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/shoptest/ShopTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shoptest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShopTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
